ed43d2deb5277e908702a152dc6bb06c888b5bd2
Daerwyn <daerwyn@icloud.com>
2023-01-08 21:09:18 +0100

    Refactored files using LoginPage

--- test_main_page.py
+++ test_main_page.py
@@ -6,6 +6,7 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    page.should_be_login_()
 
 
 def test_guest_should_see_login_lik(browser):
@@ -13,3 +14,5 @@ def test_guest_should_see_login_lik(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_
