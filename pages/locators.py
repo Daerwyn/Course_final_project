57a883b8f285a430456e561049d33fc357d9e009
Daerwyn <daerwyn@icloud.com>
2023-01-09 21:19:01 +0100

    Update base_page.py Update locators.py Update product_page.py Refactor test in test_product_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -14,6 +14,6 @@ class ProductPageLocators():
     ADD_TO_BASKET_BUTTON = (By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
     BASKET_SUM_PRICE = (By.XPATH, "//div[@id='messages']/child::*[position()=3]//p/strong")
     BASKET_SUM_PRICE_MESSAGE = (By.XPATH, "//div[@id='messages']/child::*[position()=3]")
-    PRODUCT_ADDED_TO_BASKET_MESSAGE = (By.XPATH, "//div[@id='messages']/child::*[position()=1]")
+    PRODUCT_ADDED_TO_BASKET_MESSAGE = (By.XPATH, "//div[@id='messages']/child::*[position()=1]//strong")
     PRODUCT_NAME = (By.CSS_SELECTOR, "div.col-sm-6.product_main h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
